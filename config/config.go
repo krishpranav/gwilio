0c55cd22de6b179a92e778737c5b190004ed14e0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-09 12:50:35 +0530

    config: config

--- config/config.go
+++ config/config.go
@@ -16,6 +16,21 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+type TomlConfig struct {
+	Title   string
+	Owner   Owner
+	Server  Server
+	Logging Logging
+	Fs      Freeswitch
+	Redis   Redis
+	Heartbeat Heartbeat
+	RecordingService RecordingService
+	Rating	Rating
+	Numbers Numbers
+	SipEndpoint SipEndpoint
+	Kamgo Kamgo
+}
+
 type Heartbeat struct {
 	BaseUrl string
 	UserName string
@@ -55,41 +70,45 @@ type Kamgo struct {
 
 type Owner struct {
 	Name string
-	Org string `toml:"organization"`
-	Bio string
+	Org  string `toml:"organization"`
+	Bio  string
 }
 
 type Server struct {
-	Port string
-	QueueLen int
+	Port          string
+	QueueLen      int
 	ErrorQueueLen int
-	GinMode string
+	GinMode       string
 }
 
 type Logging struct {
 	Facility string
-	Level string
-	Tag string
-	Syslog string
-	Sentry string
+	Level    string
+	Tag      string
+	Syslog   string
+	Sentry   string
 }
 
 type Freeswitch struct {
-	FsHost string
-	FsPort string
+	FsHost     string
+	FsPort     string
 	FsPassword string
-	FsTimeout int
+	FsTimeout  int
 }
 
 type Redis struct {
-
+	RedisHost     string
+	RedisPort     string
+	RedisPassword string
+	RedisDB       int
 }
+
+var Config TomlConfig
 
 func InitConfig() {
 	var err error
 
 	configFile := os.Getenv("WEBFS_CONFIG")
-
 	if len(configFile) == 0 {
 		configFile = "/etc/config.toml"
 	}
