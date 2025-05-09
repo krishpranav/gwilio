f33f991f38efc04bf96adc9741e029eed4e66e11
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-09 13:53:43 +0530

    cmd: main.go

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+/**
+ * @file main.go
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-05-09
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Gwilio: Open-Source Alternative To Twilio")
+}
