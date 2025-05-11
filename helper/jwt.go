0a826b9f211bea918216ef82078076716eb98151
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-11 12:03:53 +0530

    helper: jwt

--- helper/jwt.go
+++ helper/jwt.go
@@ -10,6 +10,13 @@
 
 package helper
 
+import (
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
 type JwtTokenInfo struct {
 	Ip string
 	Username string
@@ -21,9 +28,23 @@ type JwtTokenInfos []JwtTokenInfo
 func CreateToken(jwtInfos JwtTokenInfos) (string, error) {
 	var err error
 	
+	os.Setenv("ACCESS_SECRET", "RZ+w1Vr/dk4nZHvd/B7av/pOGiNzYlPZ") 
+	atClaims := jwt.MapClaims{}
+
+	for _, jwtInfo := range jwtInfos {
+		atClaims[jwtInfo.Ip] = jwtInfo.Username
+		atClaims[jwtInfo.Username] = jwtInfo.Password
+	}
+
+	atClaims["authorized"] = true
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+
 	if err != nil {
 		return "", err
 	}
 
 	return token, nil
-} 
+}
