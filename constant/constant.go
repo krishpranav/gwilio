bdfcf544f645c9aee6eeb75cf209e484acefdc3c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-12 11:43:18 +0530

    constant: constant

--- constant/constant.go
+++ constant/constant.go
@@ -13,9 +13,9 @@ package constant
 var gwilioConst = map[string]interface{}{
 	"GatewayDialString":"sofia/gateway/pstn_trunk",
 	"SipGatwayDialString": "sofia/gateway/pstn_trunk",
-	"DefaultVendorAuthId": "TINIYO1SECRET1AUTHID",
-	"ExportFsVars": "'tiniyo_accid,parent_call_sid,parent_call_uuid'",
-	"NumberExportFsVars": "'tiniyo_accid,parent_call_sid,parent_call_uuid,tiniyo_did_number'",
+	"DefaultVendorAuthId": "GWILIO1SECRET1AUTHID",
+	"ExportFsVars": "'gwilio_accid,parent_call_sid,parent_call_uuid'",
+	"NumberExportFsVars": "'gwilio_accid,parent_call_sid,parent_call_uuid,gwilio_did_number'",
 	"ApiVersion": "2010-04-01",
 }
 
